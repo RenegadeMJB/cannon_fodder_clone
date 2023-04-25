80f9b5a34f521d7b48aa8cb7c98cc88f68150486
Matt Bay <MJBayUNL@yahoo.com>
2023-04-25 09:52:25 -0500

    Update cannon_fodder.py

--- cannon_fodder.py
+++ cannon_fodder.py
@@ -16,9 +16,14 @@ pygame.init()
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+
+ADDENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMY,250)
+
 PlaySurf = pygame.Surface((3/4*SCREEN_WIDTH,3/4*SCREEN_HEIGHT))
 PlaySurf.fill((170,230,255))
 PlaySurfRect = PlaySurf.get_rect()
+print(PlaySurfRect.left)
 StatsSurf = pygame.Surface((SCREEN_WIDTH/4,3*SCREEN_HEIGHT/4))
 StatsSurf.fill((0,0,255))
 StatsSurfRect = StatsSurf.get_rect()
@@ -51,18 +56,31 @@ class Player(pygame.sprite.Sprite):
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super(Enemy,self).__init__()
-        self.surf = pygame.Surface(10,10)
-        self.surf.fill((255,255,255))
+        self.surf = pygame.Surface((10,10))
+        self.surf.fill((0,0,0))
         self.rect = self.surf.get_rect(
             center=(
-                random.randint(PlaySurfRect.left, PlaySurfRect.right),
+                random.randint(SCREEN_WIDTH/4, SCREEN_WIDTH),
                 5,
             )
         )
+        self.speed = random.randint(5,20)
 
+    def update(self):
+        self.rect.move_ip(0,self.speed)
+        if self.rect.bottom > PlaySurfRect.bottom:
+            self.kill()
 
 player = Player()
-player.setCoords(PlaySurfRect.centerx,PlaySurfRect.bottom)
+player.setCoords(SCREEN_WIDTH/4 + PlaySurfRect.right/2,PlaySurfRect.bottom)
+
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
+clock = pygame.time.Clock()
+
+clock.tick(30)
 
 running = True
 while running:
@@ -73,10 +91,15 @@ while running:
             if event.key == K_ESCAPE:
                 running = False
 
+        elif event.type == ADDENEMY:
+            new_enemy = Enemy()
+            enemies.add(new_enemy)
+            all_sprites.add(new_enemy)
+
     pressed_keys= pygame.key.get_pressed()
-    print(pressed_keys[K_RIGHT])
 
     player.update(pressed_keys)
+    enemies.update()
 
     screen.fill((0,255,0))
 
@@ -84,8 +107,12 @@ while running:
     screen.blit(StatsSurf, (0,0))
     screen.blit(MenuSurf, (0, 3*SCREEN_HEIGHT/4))
 
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
 
-    screen.blit(player.surf, player.rect)
+    if pygame.sprite.spritecollideany(player, enemies):
+        player.kill()
+        running = False
 
     pygame.display.flip()
 
