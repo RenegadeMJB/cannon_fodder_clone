bd68f5407d07d0a21ccf0e1adecee4a33b6aaaee
RalstonScienceApps <MJBay86@gmail.com>
2023-05-05 08:39:14 -0500

    Update pygame_rotate.py

--- pygame_rotate.py
+++ pygame_rotate.py
@@ -2,7 +2,7 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode(100,100)
+screen = pygame.display.set_mode((100,100))
 screen.fill((255,255,255))
 
 surf = pygame.Surface((100,100))
@@ -14,6 +14,10 @@ thingRect = thing.get_rect()
 thing.fill((0,0,0))
 thingRect.center = surfRect.center
 
+clock = pygame.time.Clock()
+
+angle = 0
+
 running = True
 while running:
     for event in pygame.event.get():
@@ -21,7 +25,14 @@ while running:
             running = False
     
     screen.blit(surf,(0,0))
-    screen.blit(thing, thingRect.center)
+    screen.blit(thing, (thingRect.left,thingRect.top))
+
+    angle += 1
+    if angle == 360:
+        angle = 0
+
+    thing = pygame.transform.rotate(thing,angle)
+    clock.tick(3000)
 
     pygame.display.flip()
 
