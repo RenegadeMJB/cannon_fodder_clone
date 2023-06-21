0bbcd99fffcb9893b9dd339dcafb8211c1f848a5
RalstonScienceApps <MJBay86@gmail.com>
2023-06-21 10:44:50 -0500

    Update pygame_rotate.py

--- pygame_rotate.py
+++ pygame_rotate.py
@@ -1,4 +1,57 @@
 import pygame
+import sys
+
+# Initialize Pygame
+pygame.init()
+
+# Set up the screen
+width, height = 640, 480
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Rotating Rectangle")
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# Rectangle properties
+rect_width, rect_height = 100, 50
+rect_x = width // 2 - rect_width // 2
+rect_y = height // 2 - rect_height // 2
+
+# Rotation angle
+angle = 0
+
+#clock
+clock = pygame.time.Clock()
+
+# Main game loop
+while True:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Clear the screen
+    screen.fill(WHITE)
+
+    # Rotate the rectangle
+    rotated_rect = pygame.transform.rotate(pygame.Surface((rect_width, rect_height)), angle)
+    rotated_rect_rect = rotated_rect.get_rect(center=(rect_x + rect_width // 2, rect_y + rect_height // 2))
+
+    # Draw the rotated rectangle on the screen
+    screen.blit(rotated_rect, rotated_rect_rect)
+
+    # Update the screen
+    pygame.display.flip()
+
+    # Increment the rotation angle
+    angle += 1
+    clock.tick(3)
+    if angle >= 360:
+        angle = 0
+
+'''import pygame
 
 pygame.init()
 
@@ -36,4 +89,4 @@ while running:
 
     pygame.display.flip()
 
-pygame.QUIT
+pygame.QUIT'''
